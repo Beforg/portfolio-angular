92c35c6bcd31138bf290ff637c47c7b2f1a3009d
Bruno Forgiarini <bruno.capsqn@gmail.com>
2024-10-23 20:51:34 -0300

    Organizando pastas

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -3,11 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.scss'
+  styleUrls: ['./home.component.scss']
 })
-export class HomeComponent {
-  name = "Home";
-  image = "https://via.placeholder.com/150";
-}
+export class HomeComponent {}
